2c3ccab6ba2acf4d35a977eba97d06dceedecb88
Marco Terrana <101126496+Rital24@users.noreply.github.com>
2022-03-13 17:52:10 +0100

    Add files via upload

---
+++ JavaScript/08.03/foglio.js
@@ -0,0 +1,9 @@
+
+ let firstNum = parseInt(prompt("Inserisci il primo numero: "));
+ let secondNum = parseInt(prompt("Inserisci il second numero: "));
+ let userOperation = prompt('Quale operazione vorresti svoglere?')
+ console.log(firstNum + secondNum);
+ console.log(firstNum - secondNum)
+ console.log(firstNum * secondNum)
+ console.log(firstNum / secondNum)
+ 
