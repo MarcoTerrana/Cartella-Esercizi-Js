7fe6d91b43e6f8aeddead92039615a74ba9baf11
Marco Terrana <101126496+MarcoTerrana@users.noreply.github.com>
2022-03-13 18:24:28 +0100

    Delete JavaScript directory

--- JavaScript/08.03/foglio.js
+++
@@ -1,9 +0,0 @@
-
- let firstNum = parseInt(prompt("Inserisci il primo numero: "));
- let secondNum = parseInt(prompt("Inserisci il second numero: "));
- let userOperation = prompt('Quale operazione vorresti svoglere?')
- console.log(firstNum + secondNum);
- console.log(firstNum - secondNum)
- console.log(firstNum * secondNum)
- console.log(firstNum / secondNum)
- 
