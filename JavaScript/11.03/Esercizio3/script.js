2cdbf8303b29440f4206280211ec6e7897f5d74f
Marco Terrana <101126496+MarcoTerrana@users.noreply.github.com>
2022-03-21 13:11:26 +0100

    Add files via upload

---
+++ JavaScript/11.03/Esercizio3/script.js
@@ -0,0 +1,14 @@
+ 
+ let theSecond = "Sugo di pomodoro freschissimo";
+
+function makePizza (moreIngredients) {
+   
+    // { ... }
+    console.log(theSecond + ", " + moreIngredients); 
+    return true; // non fa niente in pratica! Ritorna solo true (ne riparleremo a lezione!)
+
+}
+  if (true) {
+    makePizza("rucola, prosciutto crudo e scaglie di grana.");
+    console.log("Il secondo ingrediente necessario: ",theSecond);
+  }
